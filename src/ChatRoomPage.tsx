93b928614acb0d90cf1cff8a8b889c1eafc21c92
Hongjun Lee <zunza96@gmail.com>
2024-01-05 18:00:29 +0900

    [FEAT]: 페이지네이션 적용

--- src/ChatRoomPage.tsx
+++ src/ChatRoomPage.tsx
@@ -1,19 +1,20 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import './ChatRoomPage.css';
+import "./ChatRoomPage.css";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface ChatRoom {
   roomId: number;
 }
 function ChatRoomPage() {
   const [chatRoomList, setChatRoomList] = useState<ChatRoom[]>([]);
-
+  const [currentPage, setCurrentPage] = useState<number>(1);
   useEffect(() => {
     const loadChatRoomHistory = async () => {
       try {
-        const response = await axios.get("http://localhost:8788/api/v1/rooms");
-        const chatRoomList: ChatRoom[] = response.data.data.map((item: any) => {
+        const response = await axios.get(`http://localhost:8788/api/v1/rooms?size=10`);
+        const chatRoomList: ChatRoom[] = response.data.data.messageList.map((item: any) => {
           return { roomId: item.roomId } as ChatRoom;
         });
         setChatRoomList(chatRoomList);
@@ -23,18 +24,39 @@ function ChatRoomPage() {
     };
     loadChatRoomHistory();
   }, []);
+  const fetchData = async () => {
+    // 현재 페이지 번호를 사용하여 서버에 다음 페이지 데이터 요청
+    const response = await axios.get(
+      `http://localhost:8788/api/v1/rooms?page=${currentPage}&size=10`
+    );
+    // 데이터를 기존 리스트에 추가
+    const nextChatRoomList: ChatRoom[] = response.data.data.messageList.map((item: any) => {
+      return { roomId: item.roomId } as ChatRoom;
+    });
+    setChatRoomList([...chatRoomList, ...nextChatRoomList]);
+    setCurrentPage(currentPage + 1);
+  };
   return (
     <>
       <div className="ChatRoomPage">
-        <ul className="chatRoomList">
-          {chatRoomList.map((chatRoom, idx) => (
-            <div key={idx}>
-              <li>
-                <Link to={`/rooms/${chatRoom.roomId}`}>{chatRoom.roomId} 번 채팅방</Link>
-              </li>
-            </div>
-          ))}
-        </ul>
+        <InfiniteScroll
+          dataLength={chatRoomList.length}
+          next={fetchData}
+          hasMore={true} // 더 불러올 데이터가 있는지 여부
+          loader={<h4>Loading...</h4>}
+        >
+          <ul className="chatRoomList">
+            {chatRoomList.map((chatRoom, idx) => (
+              <div key={idx}>
+                <li>
+                  <Link to={`/rooms/${chatRoom.roomId}`}>
+                    {chatRoom.roomId} 번 채팅방
+                  </Link>
+                </li>
+              </div>
+            ))}
+          </ul>
+        </InfiniteScroll>
       </div>
     </>
   );
